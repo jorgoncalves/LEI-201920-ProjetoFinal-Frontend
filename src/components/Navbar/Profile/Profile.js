0bb03854e1415f44cdb9c4a3a4730751f74e0998
Tomás Barreira <tomas.barreira14@outlook.pt>
2020-06-02 18:04:02 +0100

    Minor updates to UI

--- src/components/Navbar/Profile/Profile.js
+++ src/components/Navbar/Profile/Profile.js
@@ -7,18 +7,21 @@ export default function Profile() {
   return (
     <div className="profileContainer">
       <a uk-icon="icon: user; ratio: 2"></a>
-      <div uk-dropdown="mode: click;">
+      <div uk-dropdown="mode: click;" className="dropdownProf">
         <ul className="uk-nav uk-dropdown-nav">
+          <li className="profInfo">--NOME--</li>
+          <li className="profInfo">--Email--</li>
+          <li className="profInfo">--Departamento--</li>
+          <li className="uk-nav-divider"></li>
           <li
             className={useLocation().pathname == '/profile' ? 'uk-active' : ''}
           >
-            <Link to="/profile" href="#">
+            <Link to="/profile" href="#" className="fLeft">
               Settings
             </Link>
           </li>
-          <li className="uk-nav-divider"></li>
           <li>
-            <a href="#">Logout</a>
+            <a href="#" className="fRight">Logout</a>
           </li>
         </ul>
       </div>
