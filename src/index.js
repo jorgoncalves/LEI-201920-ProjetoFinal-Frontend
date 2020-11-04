fb5a55af7786b1821b61496d07703e54fa047791
Jorge Gonçalves <u.jorgea@gmail.com>
2020-11-04 18:40:10 +0000

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App.js';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
