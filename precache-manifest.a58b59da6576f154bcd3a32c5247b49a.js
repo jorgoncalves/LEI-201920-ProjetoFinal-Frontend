3e0171641f72ae2b2bb9e5074f4982d90896fda4
Jorge Gonçalves <u.jorgea@gmail.com>
2020-11-04 08:08:47 +0000

    Updates

---
+++ precache-manifest.a58b59da6576f154bcd3a32c5247b49a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895229e21bb6aedfd6dc7401d41c8a36",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/index.html"
+  },
+  {
+    "revision": "c960736dc710af17ebef",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/static/css/main.afda1d35.chunk.css"
+  },
+  {
+    "revision": "a82621e55aa066c4b063",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/static/js/2.b1a8108f.chunk.js"
+  },
+  {
+    "revision": "247eded03404b08379d0400d8be22b92",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/static/js/2.b1a8108f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c960736dc710af17ebef",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/static/js/main.aac91edc.chunk.js"
+  },
+  {
+    "revision": "c60412a7eb3790b1af9d",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/static/js/runtime-main.6f134611.js"
+  },
+  {
+    "revision": "104c0a33c873970345d569bef4823835",
+    "url": "/LEI-201920-ProjetoFinal-Frontend/static/media/LOGO.104c0a33.png"
+  }
+]);
